95f93c386c3c26515c9041e723b2bf3e7dfdf465
thopiyawan <tho.piyawan@gmail.com>
2026-05-08 11:14:40 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -4512,8 +4512,8 @@ private function analyzeImageWithGemini($imageBinary)
 {
     try {
         $projectId = config('services.google_cloud.project_id'); // ตั้งค่าใน config/services.php
-        $location = 'asia-southeast1'; 
-        $modelId = 'gemini-1.5-flash'; // Vertex AI แนะนำตัวนี้สำหรับงานความเร็วสูง
+        $location = 'us-central'; 
+        $modelId = 'gemini-1.5-flash-002'; // Vertex AI แนะนำตัวนี้สำหรับงานความเร็วสูง
 
         // 1. สร้าง Access Token จาก Service Account JSON (ไฟล์ที่คุณลงไว้)
         $credentials = new ServiceAccountCredentials(

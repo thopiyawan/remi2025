9ffc65f1e419b835342cd1320c2794c7aeed540f
thopiyawan <tho.piyawan@gmail.com>
2026-06-15 14:56:36 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -4649,7 +4649,7 @@ private function formatLineResponse($data)
     foreach ($foods as $food) {
 
         $msg .= "🍽️ " . ($food['name'] ?? '-') . "\n";
-        $msg .= "🍽️ " . ($full_foodname ?? '-') . "\n";
+        $msg .= "🍽️ " . ($data['full_foodname'] ?? '-') . "\n";
 
         if (!empty($food['ingredients'])) {
 
